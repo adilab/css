b242be424d625134b8f76fe9b89419ce2fe33a2b
Adrian Zurkiewicz <adrian.zurkiewicz@gmail.com>
2016-04-15 19:41:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Adi CSS Tools for PHP
+========================
+
+Adi CSS Tools for PHP is a very intuitive tool for prepare CSS strings directly in PHP code.
+
+Installing
+----------
+
+Preferred way to install is with [Composer](https://getcomposer.org/).
+
+Install this library using composer:
+
+```console
+$ composer require adilab/css
+```
+
---
+++ src/Css.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ *
+ * AdiPHP : Rapid Development Tools (http://adilab.net)
+ * Copyright (c) Adrian Zurkiewicz
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @version     0.1
+ * @copyright   Adrian Zurkiewicz
+ * @link        http://adilab.net
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Adi\Css;
+
+use Exception;
+
+/**
+ * Css handler.
+ *
+ * @author adrian
+ */
+class Css {
+
+	private $css = array();
+
+	/**
+	 * Constructor
+	 *
+	 * <code>
+	 * $css = new Css('display: inline-block;margin-right: 10px;min-width: 150px;');
+	 * $css->set('width: 20px');
+	 * $css->set('color', '#ff0000');
+	 * echo $css;
+	 * </code>
+	 * 
+	 * @param string $css CSS string
+	 */
+	function __construct($css = NULL) {
+
+		$css = trim(strtolower($css));
+
+		if ($css) {
+			$this->set($css);
+		}
+	}
+
+	/**
+	 * Removes CSS property.
+	 *
+	 * <code>
+	 * $css->remove('width');
+	 * </code> 
+	 *
+	 * @param string $property
+	 */
+	public function remove($property) {
+
+		$property = trim(strtolower($property));
+
+		unset($this->css[$property]);
+	}
+
+	/**
+	 * Sets CSS value.
+	 *
+	 * <code>
+	 * $css->set('width: 20px');
+	 * $css->set('color', '#ff0000');
+	 * </code>
+	 *
+	 * @param mixed $css CSS property or CSS expression string
+	 * @param string $value
+	 * 
+	 */
+	public function set($css, $value = NULL) {
+
+		$css = trim(strtolower($css));
+
+
+		if (func_num_args() == 1) {
+
+			$elements = explode(';', $css);
+
+			foreach ($elements as $element) {
+
+				if (!$element = trim($element)) {
+					continue;
+				}
+
+				$element = explode(':', $element);
+
+				if (!$element[0] = trim(@$element[0])) {
+					continue;
+				}
+
+				$element[1] = trim(@$element[1]);
+
+				$this->set($element[0], $element[1]);
+			}
+
+			return;
+		}
+
+		$css = strtolower($css);
+		$this->css[$css] = $value;
+	}
+
+	/**
+	 * Returns CSS value
+	 *
+	 * @param string $property
+	 * @return string 
+	 */
+	public function get($property) {
+
+		$property = trim(strtolower($property));
+
+		return @$this->css[$property];
+	}
+
+	/**
+	 * Creates CSS string
+	 *
+	 * @return string
+	 * 
+	 */
+	public function render() {
+
+		$result = NULL;
+
+		foreach ($this->css as $name => $value) {
+
+			if (!$value = trim($value)) {
+				continue;
+			}
+
+			$result .= "{$name}:{$value};";
+		}
+
+		return $result ? $result : '';
+	}
+
+	/**
+	 * Casts to string.
+	 *
+	 * @return string
+	 * 
+	 */
+	public function __toString() {
+
+		return $this->render();
+	}
+
+}
